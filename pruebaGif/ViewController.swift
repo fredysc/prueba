5ead64bdc601183247c32f14571f4847adbdd673
FREDY SALGADO <fredysc@gmail.com>
2015-10-25 18:41:05 -0600

    commit

--- pruebaGif/ViewController.swift
+++ pruebaGif/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+//modificacion  1
+
+override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
